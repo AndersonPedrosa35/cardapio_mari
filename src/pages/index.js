38b7c164678ca81f74a3907e6000108022d9d263
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2023-07-14 09:59:03 -0300

    Feat: Atualizando cardapio 14/07

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ export default function Home() {
       className='h-full w-full bg-slate-200 flex justify-center items-center'
     >
       <Image
-        src="/cardapio.jpg"
+        src="/cardapio_14_07.jpg"
         alt="Cardapio"
-        className="h-full w-full"
+        className="h-full w-full image-cardapio"
         width={500}
         height={1000}
         priority
